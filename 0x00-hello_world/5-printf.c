740e1147da9080fa6e9dc90a449f0b30174090e8
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-07 09:12:55 -0700

    my project 5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is my entry point
+ *
+ * Return: if this returns (0),it is a success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
