9e7fd272d406a65bba01189ebfebcf7ea454eea1
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-07 08:58:52 -0700

    my betty prject 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the code entrance
+ *
+ * Return: if the code returns (0) ,it is successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
