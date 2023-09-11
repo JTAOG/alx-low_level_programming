eaa05ed39667702365898ab630d7f69eb5b2eaad
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-11 04:28:26 -0700

    my alx putchar project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is my enrty point
+ *
+ * Return: (0) is a success
+ */
+int main(void)
+{
+	char f;
+
+	for (f = 'a'; f < 'z'; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n');
+	return (0);
+}
