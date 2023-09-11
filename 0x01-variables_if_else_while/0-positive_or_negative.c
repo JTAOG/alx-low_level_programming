beafcc5b596e0833992479849834561c97b5ac5b
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-11 03:43:55 -0700

    my alx 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is the entry of the code
+ *
+ * Return: (0) means success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
