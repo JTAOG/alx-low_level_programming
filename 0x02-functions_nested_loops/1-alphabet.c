da76ee6390844ae53d19accb46214724cdc981ef
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-12 07:23:31 -0700

    my _putchar project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - this is my entry point
+ *
+ * Return: (0) is a success
+ */
+int main(void)
+{
+	char h;
+	for (h = 'a'; h <= 'z'; h++)
+		_putchar(h);
+	putchar('\n');
+	return (0);
+}
