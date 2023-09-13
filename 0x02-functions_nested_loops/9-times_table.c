adeb68445e35d8134f18118d48c4000769fb47bf
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-13 04:57:24 -0700

    my alx project works

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,30 +12,29 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			k = i * j;
-			if (k > 9)
-			{
-				l = k % 10;
-				m = (l - k) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(m + '0');
-				_putchar(l + '0');
-			}
-			else
-			{
-				if (j != 0)
-				{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				}
-				_putchar(k + '0');
-			}
-		}
-		_putchar('\n');
+	for (j = 0; j <= 9; j++)
+	{
+	k = i * j;
+	if (k > 9)
+	{
+	l = k % 10;
+	m = (k - l) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(m + '0');
+	_putchar(l + '0');
 	}
-
+	else
+	{
+	if (j != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(k + '0');
+	}
+	}
+	_putchar('\n');
+	}
 }
