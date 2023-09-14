b89029ca0ce9a54de2601199507bc18b80a86094
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-14 02:18:45 -0700

    my first debugging project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - this is to test for positive and negative functions
+ *
+ * Return: (0) is a success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
