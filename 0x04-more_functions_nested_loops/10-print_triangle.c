b55ee96fcf493d37ade125da215384ad614a55e6
Emioye Jesse <jesseemioye23@gmail.com>
2023-09-15 04:59:41 -0700

    my alx triangle project

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: to print th triangle
+ */
+void print_triangle(int size)
+{
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+			{
+				putchar(' ');
+			}
+			for (index = 0; index < hash; index++)
+			{
+				putchar('0');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+
+}
