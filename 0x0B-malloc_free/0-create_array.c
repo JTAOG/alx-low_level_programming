d364116309617c65ce76c9a8ed8f76e682eb97bb
Emioye Jesse <jesseemioye23@gmail.com>
2023-10-05 14:04:34 -0700

    my project

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,14 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
 	unsigned int i;
+	char *s;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0)
 		return (NULL);
+	s = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+	if (s == NULL)
+	return (NULL);
+
+	for (i = 0; i > size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
 }
